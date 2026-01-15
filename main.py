d244b32e655a194ba2e7505b7f4e281ab625b5b8
79h7kwwh2w-droid <79h7kwwh2w@privaterelay.appleid.com>
2026-01-15 12:34:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('영화 이건 꼭 보고 넘어가자           (Title and star)')
+st.title('영화 이건 꼭 보고 넘어가자(Title and star)')
 
 # 사용자 입력 받기
 movie_property = st.selectbox('원하는 영화 특징을 선택하세요', [
@@ -8,8 +8,8 @@ movie_property = st.selectbox('원하는 영화 특징을 선택하세요', [
 ])
 
 # 영화제목과 평점
-movie_data = {
-    '여운형': {
+movie_data = 
+    '여운형': 
         'Aftersun': ' 4.2 ',
         'F1(2025)':'3.7',
         'Little Amelie or the Character of Rain(2025)':'4.1',
@@ -18,22 +18,22 @@ movie_data = {
         'Joker(2019)':'3.8',
         'Marriage Story(2019)':'4.0',
         '기적(2021)':'3.7'
-    },
-    '해피엔딩': {
+    ,
+    '해피엔딩': 
         'The Match(2025)':'3.4',
         'Wicked(2024)':'3.8',
         'The Intern(2015)':'3.3',
         'The Truman Show(1998)':'4.2',
         'The Notebook(2004)':'3.9'
      
-    },
-    '열린결말': {
+    ,
+    '열린결말': 
         'Happyend(2024)':'4.0',
         'Decision to Leave':'4.0',
         'Nothing Serious':'3.2',
         'No Country for old Men(2007)':'4.3'
        
-    },
+    ,
     '불편한 엔딩': {
         'Michey 17(2025)':'3.6',
         'Super Happy Forever(2024)':'3.6',
